7b75ea5127d0a7139efd0d3057fbb2933ea0aa0d
Asger Juul Brunshøj <asgerbrunshoj@gmail.com>
2021-03-03 18:35:10 +0100

    make into workspace and move the binary to "playground" crate

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,12 +1,14 @@
-/// This is just a playground to play with it while developing.
-///
-/// When the library is finished, this file shall be removed.
+//! This is just a playground to play with it while developing.
+//!
+//! When the library is finished, this file shall be removed.
+
+use dbus_stream::Connection;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("debug")).init();
 
     smol::block_on(async {
-        let system = dbus::connection::Connection::new_system().await;
+        let system = Connection::new_system().await;
 
         system
     })?;
