78145a69185c645e9a06b7cd74a069e3d2c4916e
Daniel Mancebo Aldea <daniel.m.aldea@hotmail.com>
2021-03-09 20:23:47 +0100

    basic types are now unmarshalled

--- dbus-stream/src/type_system/unmarshall.rs
+++ dbus-stream/src/type_system/unmarshall.rs
@@ -3,39 +3,79 @@ use std::convert::TryFrom;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take;
 use nom::combinator::map_res;
+use nom::number::complete::be_f64;
+use nom::number::complete::be_i16;
+use nom::number::complete::be_i32;
+use nom::number::complete::be_i64;
+use nom::number::complete::be_u16;
 use nom::number::complete::be_u32;
+use nom::number::complete::be_u64;
+use nom::number::complete::be_u8;
 use nom::number::complete::le_u32;
+use nom::Err;
 use nom::IResult;
 
 use super::types::*;
 
 impl DBusByte {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_u8(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { u8: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
 impl DBusBoolean {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_u32(i)?;
+
+        // Only 0 or 1 are valid values.
+        if value > 1 {
+            todo!("Don't know how to return an error")
+        }
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self {
+            bool: if value == 1 { true } else { false },
+        };
+
+        Ok((i, unmarshalled))
     }
 }
 
 impl DBusInt16 {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_i16(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { i16: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
 impl DBusUint16 {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_u16(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { u16: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
 impl DBusInt32 {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_i32(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { i32: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
@@ -53,19 +93,34 @@ impl DBusUint32 {
 
 impl DBusInt64 {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_i64(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { i64: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
 impl DBusUint64 {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_u64(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { u64: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
 impl DBusDouble {
     fn unmarshall_be(i: &[u8]) -> IResult<&[u8], Self> {
-        todo!("Dani");
+        let (i, value) = be_f64(i)?;
+
+        // Create a Self type, (the unmarshalled type).
+        let unmarshalled: Self = Self { f64: value };
+
+        Ok((i, unmarshalled))
     }
 }
 
