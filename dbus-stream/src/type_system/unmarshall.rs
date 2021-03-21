bdf8e33b79fa0d36a8b1c649ab61fad36c8558c6
Asger Juul Brunshøj <asgerbrunshoj@gmail.com>
2021-03-21 18:16:05 +0100

    Unmarshall trait and Decoder struct

--- dbus-stream/src/type_system/unmarshall.rs
+++ dbus-stream/src/type_system/unmarshall.rs
@@ -1,3 +1,9 @@
+use std::iter::Copied;
+use std::iter::Enumerate;
+use std::iter::Iterator;
+use std::ops::RangeFrom;
+use std::slice::Iter;
+
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take;
 use nom::combinator::all_consuming;
@@ -14,6 +20,8 @@ use nom::number::complete::be_u8;
 use nom::number::complete::le_u32;
 use nom::Finish;
 use nom::IResult;
+use nom::InputIter;
+use nom::Needed;
 
 use super::signature::SingleCompleteTypeSignature;
 use super::signature::HEADER_FIELD_SIGNATURE;
@@ -21,6 +29,178 @@ use super::types::*;
 use super::Endianness;
 use crate::message_protocol::Message;
 use crate::message_protocol::MessageType;
+
+trait Unmarshal: Sized {
+    fn unmarshall_be<'a>(i: Decoder<'a>) -> IResult<Decoder<'a>, Self>;
+}
+
+/// A wrapper over `&[u8]` but with the ability to keep track global alignment.
+#[derive(Copy, Clone, Debug)]
+struct Decoder<'a> {
+    /// The unmodified original slice the Decoder was created with.
+    /// This is assumed to begin on an 8-byte boundary.
+    ///
+    /// This is used for global alignment.
+    original_data: &'a [u8],
+
+    /// Slice that is chipped away at, like a normal input as it passes through nom parsers.
+    data: &'a [u8],
+}
+
+impl<'a> Decoder<'a> {
+    /// Create new [Decoder] from raw data.
+    ///
+    /// The data slice must be globally aligned so it starts on an 8-byte boundary!
+    fn new(data: &'a [u8]) -> Self {
+        Self {
+            original_data: data,
+            data: data.clone(),
+        }
+    }
+
+    /// Skip over padding (null bytes) until at an n-byte boundary (relative to global alignment).
+    fn advance_to_boundary(
+        self,
+        boundary: usize,
+    ) -> Result<Decoder<'a>, nom::Err<nom::error::Error<Decoder<'a>>>> {
+        let i = self;
+
+        // Don't really expect to need this for other boundaries than 2, 4 and 8.
+        debug_assert!([1, 2, 4, 8].contains(&boundary), "Sanity check");
+
+        // Original slice is guaranteed to start on 8-byte boundary.
+        let offset = while (i.original_data.len() - i.data.len()) % boundary != 0 {
+            // Eat a null byte.
+            let (i, _) = tag(&[0])(i)?;
+        };
+
+        Ok(i)
+    }
+}
+
+impl<'a> nom::Slice<RangeFrom<usize>> for Decoder<'a> {
+    fn slice(&self, range: RangeFrom<usize>) -> Self {
+        Self {
+            original_data: &self.original_data,
+            data: &self.data[range],
+        }
+    }
+}
+
+impl<'a> nom::InputIter for Decoder<'a> {
+    type Item = u8;
+    type Iter = Enumerate<Self::IterElem>;
+    type IterElem = Copied<Iter<'a, u8>>;
+
+    fn iter_indices(&self) -> Self::Iter {
+        self.data.iter_indices()
+    }
+
+    fn iter_elements(&self) -> Self::IterElem {
+        self.data.iter_elements()
+    }
+
+    fn position<P>(&self, predicate: P) -> Option<usize>
+    where
+        P: Fn(Self::Item) -> bool,
+    {
+        self.data.position(predicate)
+    }
+
+    fn slice_index(&self, count: usize) -> Result<usize, Needed> {
+        self.data.slice_index(count)
+    }
+}
+
+impl<'a> nom::InputLength for Decoder<'a> {
+    fn input_len(&self) -> usize {
+        self.data.input_len()
+    }
+}
+
+impl<'a> nom::InputTake for Decoder<'a> {
+    fn take(&self, count: usize) -> Self {
+        Self {
+            original_data: self.original_data,
+            data: self.data.take(count),
+        }
+    }
+
+    fn take_split(&self, count: usize) -> (Self, Self) {
+        let (a, b) = self.data.take_split(count);
+
+        let a = Self {
+            original_data: self.original_data,
+            data: a,
+        };
+
+        let b = Self {
+            original_data: self.original_data,
+            data: b,
+        };
+
+        (a, b)
+    }
+}
+
+impl<'a, 'b> nom::Compare<&'b [u8]> for Decoder<'a> {
+    fn compare(&self, t: &'b [u8]) -> nom::CompareResult {
+        self.data.compare(t)
+    }
+
+    fn compare_no_case(&self, t: &'b [u8]) -> nom::CompareResult {
+        self.data.compare_no_case(t)
+    }
+}
+
+// Replace this shit with const generics whenever the underlying nom stuff is updated with const generics.
+impl<'a, 'b> nom::Compare<&'b [u8; 1]> for Decoder<'a> {
+    fn compare(&self, t: &'b [u8; 1]) -> nom::CompareResult {
+        self.data.compare(t)
+    }
+
+    fn compare_no_case(&self, t: &'b [u8; 1]) -> nom::CompareResult {
+        self.data.compare_no_case(t)
+    }
+}
+
+// // The const generics version:
+// impl<'a, 'b, const N: usize> nom::Compare<&'b [u8; N]> for Decoder<'a> {
+//     fn compare(&self, t: &'b [u8; N]) -> nom::CompareResult {
+//         self.data.compare(t)
+//     }
+
+//     fn compare_no_case(&self, t: &'b [u8; N]) -> nom::CompareResult {
+//         self.data.compare_no_case(t)
+//     }
+// }
+
+impl Unmarshal for DBusByte {
+    fn unmarshall_be<'a>(i: Decoder<'a>) -> IResult<Decoder<'a>, Self> {
+        let (i, value) = be_u8(i)?;
+
+        let unmarshalled: Self = Self { u8: value };
+
+        Ok((i, unmarshalled))
+    }
+}
+
+impl Unmarshal for DBusBoolean {
+    fn unmarshall_be<'a>(i: Decoder<'a>) -> IResult<Decoder<'a>, Self> {
+        let i = i.advance_to_boundary(4)?;
+
+        // The boolean is contained in a u32, but only 0 or 1 are valid values.
+        let (i, bool): (Decoder, bool) = map_opt(be_u32, |value| match value {
+            0 => Some(false),
+            1 => Some(true),
+            _ => None,
+        })(i)?;
+
+        let unmarshalled: Self = Self { bool };
+
+        Ok((i, unmarshalled))
+    }
+}
 
 /// Unmarshall a DBus message (consisting of header and body),
 pub fn unmarshall_message(message: &[u8]) -> crate::Result<Message> {
